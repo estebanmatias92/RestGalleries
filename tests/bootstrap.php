a52bce2962818ddfcdee975c58ca6e83fc9c7712
estebanmatias92 <estebanmatias92@gmail.com>
2014-02-21 01:40:09 -0300

    Adding Travis CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,3 @@
 <?php
-
-if ( ! @include_once __DIR__ . '/../vendor/autoload.php')
-{
-    exit("You must set up the project dependencies, run the following commands:\n> wget http://getcomposer.org/composer.phar\n> php composer.phar install\n");
-}
+var_dump(__DIR__ . '/../vendor/autoload.php'); // BORRAR !!!!!
+require_once __DIR__ . '/../vendor/autoload.php';
