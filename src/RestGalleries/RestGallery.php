73e9e9739669f52c7d6b9d9acefd84cdc2668c64
estebanmatias92 <estebanmatias92@gmail.com>
2014-06-24 21:03:14 -0300

    RestGallery model documented.

--- src/RestGalleries/RestGallery.php
+++ src/RestGalleries/RestGallery.php
@@ -2,24 +2,59 @@
 
 use RestGalleries\Factory;
 
+/**
+ * Abstract model CRUD-type to simplify the interaction with api services.
+ */
 abstract class RestGallery
 {
+    /**
+     * Api service name.
+     *
+     * @var string
+     */
     protected $service;
 
+    /**
+     * Object to make the api requests.
+     *
+     * @var object
+     */
     protected $query;
 
+    /**
+     * Credentials to authenticate every api request,
+     *
+     * @var array
+     */
     protected $credentials = [];
 
+    /**
+     * Returns all galleries (and its photos) from an api service.
+     *
+     * @return \Illuminate\Support\Collection|null
+     */
     public function all()
     {
         return $this->query->all();
     }
 
+    /**
+     * Finds and returns a specific gallery from an api service.
+     *
+     * @param  string  $id
+     * @return \Illuminate\Support\Fluent|null
+     */
     public function find($id)
     {
         return $this->query->find($id);
     }
 
+    /**
+     * Makes the authentication for the requests,
+     *
+     * @param  array  $tokenCredentials
+     * @return object
+     */
     public static function authenticate(array $tokenCredentials)
     {
         $instance        = new static;
@@ -33,6 +68,11 @@ abstract class RestGallery
 
     }
 
+    /**
+     * This method create a new service gallery object to interact with the external api service.
+     *
+     * @return RestGalleries\APIs\<service>\Gallery
+     */
     public function newQuery()
     {
         $class = $this->getService() . '\\Gallery';
@@ -41,6 +81,12 @@ abstract class RestGallery
 
     }
 
+    /**
+     * Takes account credentials and calls the user object to make the requests to get the api authorization.
+     *
+     * @param  array  $clientCredentials
+     * @return \Illuminate\Support\Fluent
+     */
     public static function connect(array $clientCredentials)
     {
         $instance = new static;
@@ -52,6 +98,12 @@ abstract class RestGallery
 
     }
 
+    /**
+     * Investigates if the access tokens still are valid. calling the user object and making the respective requests,
+     *
+     * @param  array  $tokenCredentials
+     * @return \Illuminate\Support\Fluent
+     */
     public static function verifyCredentials(array $tokenCredentials)
     {
         $instance = new static;
@@ -63,6 +115,11 @@ abstract class RestGallery
 
     }
 
+    /**
+     * Creates new user object that will interact with the api service.
+     *
+     * @return RestGalleries\APIs\<service>\User
+     */
     public function newAuthentication()
     {
         $class = $this->getService() . '\\User';
@@ -71,22 +128,44 @@ abstract class RestGallery
 
     }
 
+    /**
+     * Returns current credential array.
+     *
+     * @return array
+     */
     public function getCredentials()
     {
         return $this->credentials;
     }
 
+    /**
+     * Takes adds and cleans credentials of an account.
+     *
+     * @param  array  $credentials
+     * @return void
+     */
     public function setCredentials(array $credentials)
     {
         $this->credentials = $this->addCredentials($credentials);
         $this->credentials = $this->cleanCredentials();
     }
 
+    /**
+     * Adds more credentials to the current credential array.
+     *
+     * @param  array  $credentials
+     * @return array
+     */
     private function addCredentials($credentials)
     {
         return array_merge($this->getCredentials(), $credentials);
     }
 
+    /**
+     * Cleans current credential array of empty values and repeated keys.
+     *
+     * @return array
+     */
     private function cleanCredentials()
     {
         $filtered = array_filter($this->getCredentials());
@@ -95,6 +174,11 @@ abstract class RestGallery
 
     }
 
+    /**
+     * If service name is configured, return it, but returns the child class name for the service api name.
+     *
+     * @return string
+     */
     public function getService()
     {
         if (isset($this->service)) {
@@ -105,8 +189,15 @@ abstract class RestGallery
 
     }
 
+    /**
+     * Sets the current service api name to interact with it.
+     *
+     * @param  string  $service
+     * @return void
+     */
     public function setService($service)
     {
         $this->service = $service;
     }
+
 }
